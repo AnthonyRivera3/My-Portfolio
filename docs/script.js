3bb2445ef09bfe43221f51c16873790df424e984
AnthonyRivera3 <Anthony.rivera01110@gmail.com>
2025-02-21 13:05:30 -0500

    making button clickable including js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,13 @@
+function toggleMobileMenu() {
+    var navbar = document.getElementById("navbarNavAltMarkup");
+    
+   
+    if (window.innerWidth <= 768) {
+        if (navbar.classList.contains("show")) {
+            navbar.classList.remove("show"); 
+        } else {
+            navbar.classList.add("show"); 
+        }
+    }
+}
 
